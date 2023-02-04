0d1a751d9020f359a10479743b14ff1f92c99dd0
Tyler Skidmore <tyler.s9@proton.me>
2023-02-04 13:48:42 -0500

    calculate payment code

--- calculator.js
+++ calculator.js
@@ -40,15 +40,24 @@ function setupIntialValues() {
 // Update the monthly payment
 function update() {
   let currentUIValues = getCurrentUIValues();
+  updateMonthly(calculateMonthlyPayment(currentUIValues));
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  let monthlyRate = (values.rate / 100) / 12;
+  let n = Math.floor(values.years * 12);
+
+  return (
+    (monthlyRate * values.amount) / (1 - Math.pow((1 + monthlyRate), -n))
+  );
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  let monthlyPayment = document.getElementById("monthly-payment");
+  monthlyPayment.innerText = `$ ${monthly}`;
 }
