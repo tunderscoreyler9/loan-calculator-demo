9ce03c2b1c531cc20f7a16ea8957d4199d94a624
Tyler Skidmore <tyler.s9@proton.me>
2023-02-04 13:24:59 -0500

    initial commit for calculator

---
+++ calculator.js
@@ -0,0 +1,51 @@
+window.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById("calc-form");
+  if (form) {
+    setupIntialValues();
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      update();
+    });
+  }
+});
+
+function getCurrentUIValues() {
+  return {
+    amount: +(document.getElementById("loan-amount").value),
+    years: +(document.getElementById("loan-years").value),
+    rate: +(document.getElementById("loan-rate").value),
+  }
+}
+
+// Get the inputs from the DOM.
+// Put some default values in the inputs
+// Call a function to calculate the current monthly payment
+function setupIntialValues() {
+  let loanAmountInput = document.getElementById('loan-amount');
+  let loanYearsInput = document.getElementById('loan-years');
+  let loanRateInput = document.getElementById('loan-rate');
+
+  let loanValue = loanAmountInput.value = 10000;
+  let loanLength = loanYearsInput.value = 5;
+  let loanRate = loanRateInput.value = 6.99;
+
+  function calculateMonthlyPayment(values) {
+    let values = 
+  }
+}
+
+// Get the current values from the UI
+// Update the monthly payment
+function update() {
+}
+
+// Given an object of values (a value has amount, years and rate ),
+// calculate the monthly payment.  The output should be a string
+// that always has 2 decimal places.
+function calculateMonthlyPayment(values) {
+}
+
+// Given a string representing the monthly payment value,
+// update the UI to show the value.
+function updateMonthly(monthly) {
+}
